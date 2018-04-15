649feca118b8b8cbc6f4c87da1da9c7f0d277053
Estelle <EstelleWu@git.com>
2018-04-14 20:46:18 -0700

    Initial Commit

---
+++ MusicVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicVideo
+//
+//  Created by Estelle qiyu on 2018-04-14.
+//  Copyright © 2018 Estelle qiyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
